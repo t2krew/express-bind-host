0a8754ed889b19f46cabe8d7f0bb602966b8a951
t2krew <wanhao.huang@outlook.com>
2017-03-24 21:40:03 +0800

    REFINE

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 'use strict';
 
-
+/**
+ * @param {Object} opts Config
+ * @param {Array|String} opts.hosts Available host list (required)
+ * @param {String} opts.warning Warning text (optional)
+ */
 module.exports = function BindHost(opts) {
   opts = opts || {};
   if (!opts.hosts) {
     throw new TypeError('option must contain hosts');
   }
-
-  let $all = false,
-      hosts = opts.hosts;
- 
+  let $all = false,hosts = opts.hosts;
+  
   if (typeof hosts === 'string' && hosts !== '*') {
     hosts = [hosts];
   } else if (hosts === '*') {
